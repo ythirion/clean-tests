c87c2a16eece14bbc97997342689c837fd5deca0
Yoan Thirion <yoan.thirion@gmail.com>
2021-09-24 13:35:12 +0200

    clean tests

--- src/test/scala/demo/customer/LondonCustomerTests.scala
+++ src/test/scala/demo/customer/LondonCustomerTests.scala
@@ -1,42 +1,31 @@
 package demo.customer
 
+import demo.customer.ProductType.ProductType
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 
 class LondonCustomerTests extends AnyFlatSpec with MockFactory {
-  it should "purchase successfully when enough inventory" in {
-    // Arrange
-    val productType = ProductType.Book
-    val quantity = 6
-    val storeStub = stub[Store]
+  val productType: ProductType = ProductType.Book
+  val quantity = 6
+  val storeStub: Store = stub[Store]
 
+  it should "purchase successfully when enough inventory" in {
     (storeStub.hasEnoughInventory _)
       .when(productType, quantity)
       .returns(true)
 
-    // Act
-    val updatedStore =
-      CustomerService.purchase(storeStub, productType, quantity)
+    CustomerService.purchase(storeStub, productType, quantity)
 
-    // Assert
     (storeStub.removeInventory _).verify(productType, quantity).once()
   }
 
   it should "fail to purchase when not enough inventory" in {
-    // Arrange
-    val productType = ProductType.Book
-    val quantity = 6
-    val storeStub = stub[Store]
-
     (storeStub.hasEnoughInventory _)
       .when(productType, quantity)
       .returns(false)
 
-    // Act
-    val updatedStore =
-      CustomerService.purchase(storeStub, productType, quantity)
+    CustomerService.purchase(storeStub, productType, quantity)
 
-    // Assert
     (storeStub.removeInventory _).verify(productType, quantity).never()
   }
 }
